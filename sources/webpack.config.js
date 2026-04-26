469acaa5f1d5eaafa5cb286f7413f89f18e976bd
ldu1991 <ldu2601@gmail.com>
2026-04-26 21:19:16 +0300

    Bugs fix webpack

--- sources/webpack.config.js
+++ sources/webpack.config.js
@@ -1,73 +1,71 @@
 import path from 'path';
 import TerserPlugin from 'terser-webpack-plugin';
-import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import { globSync } from 'glob';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname  = path.dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 export default (env, argv) => {
-    let entry = {};
+  let entry = {};
 
-    globSync('./js/*.js').forEach((file) => {
-        entry[path.join('assets', file)] = './' + file;
-    });
+  globSync('./js/*.js').forEach((file) => {
+    entry[path.join('assets', file)] = './' + file;
+  });
 
-    globSync('./blocks/**/*.js').forEach((file) => {
-        if (!file.includes('__example')) {
-            entry[file] = './' + file;
-        }
-    });
-
-    let config = {
-        mode:   argv.mode,
-        entry:  entry,
-        output: {
-            filename: '[name]',
-            path:     path.resolve(__dirname, '../')
-        },
-        module: {
-            rules: [
-                {
-                    test:    /\.js$/,
-                    exclude: /node_modules/,
-                    use:     {
-                        loader:  'babel-loader',
-                        options: {
-                            presets: ['@babel/preset-env']
-                        }
-                    }
-                }
-            ]
-        }
-    };
-
-    if (argv.mode === 'production') {
-        config.optimization = {
-            minimize:  true,
-            minimizer: [
-                new TerserPlugin({
-                    terserOptions:   {
-                        ecma:   5,
-                        format: {
-                            comments: false
-                        }
-                    },
-                    extractComments: false
-                })
-            ]
-        };
-        config.plugins      = [new CleanWebpackPlugin()];
-        config.performance  = {
-            hints: false
-        };
-    } else {
-        config.watch = true;
-        config.cache = {
-            type: 'filesystem'
-        };
+  globSync('./blocks/**/*.js').forEach((file) => {
+    if (!file.includes('__example')) {
+      entry[file] = './' + file;
     }
+  });
 
-    return config;
-}
+  let config = {
+    mode: argv.mode,
+    entry: entry,
+    output: {
+      filename: '[name]',
+      path: path.resolve(__dirname, '../'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
+    },
+  };
+
+  if (argv.mode === 'production') {
+    config.optimization = {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            ecma: 5,
+            format: {
+              comments: false,
+            },
+          },
+          extractComments: false,
+        }),
+      ],
+    };
+    config.performance = {
+      hints: false,
+    };
+  } else {
+    config.watch = true;
+    config.cache = {
+      type: 'filesystem',
+    };
+  }
+
+  return config;
+};
